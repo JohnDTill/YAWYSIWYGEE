54372239f35884397296192d232c33ad27bd5d6f
John Till <johndtill@gmail.com>
2020-04-10 14:10:14 -0700

    Added adhoc shortcuts

--- TypesetWidget/cursor_adhoc_sub.cpp
+++ TypesetWidget/cursor_adhoc_sub.cpp
@@ -27,6 +27,12 @@ void Cursor::checkForSubstitution(const QChar& c){
         case '_':
             checkTwoCharSub<60,8804>(*this, cursor);
             checkTwoCharSub<62,8805>(*this, cursor);
+            checkTwoCharSub<61,8801>(*this, cursor);
+            checkTwoCharSub<8801,8803>(*this, cursor);
+            checkTwoCharSub<43,177>(*this, cursor);
+            checkTwoCharSub<8847,8849>(*this, cursor);
+            checkTwoCharSub<8848,8850>(*this, cursor);
+            checkTwoCharSub<8744,8891>(*this, cursor);
             break;
         case '-':
             checkTwoCharSub<60,8592>(*this, cursor);
@@ -34,6 +40,7 @@ void Cursor::checkForSubstitution(const QChar& c){
             break;
         case ':':
             checkTwoCharSub<58,8759>(*this, cursor);
+            checkTwoCharSub<45,8761>(*this, cursor);
             break;
         case '/':
             checkTwoCharSub<92,8744>(*this, cursor);
@@ -43,9 +50,22 @@ void Cursor::checkForSubstitution(const QChar& c){
             checkTwoCharSub<8834,8836>(*this, cursor);
             checkTwoCharSub<60,8814>(*this, cursor);
             checkTwoCharSub<62,8815>(*this, cursor);
+            checkTwoCharSub<8801,8802>(*this, cursor);
+            checkTwoCharSub<8776,8777>(*this, cursor);
+            checkTwoCharSub<8773,8775>(*this, cursor);
+            checkTwoCharSub<8801,8802>(*this, cursor);
+            checkTwoCharSub<8715,8716>(*this, cursor);
+            checkTwoCharSub<8834,8836>(*this, cursor);
+            checkTwoCharSub<8838,8840>(*this, cursor);
+            checkTwoCharSub<8835,8837>(*this, cursor);
+            checkTwoCharSub<8839,8841>(*this, cursor);
             break;
         case '\\':
             checkTwoCharSub<47,8743>(*this, cursor);
+            break;
+        case '<':
+            checkTwoCharSub<60,8810>(*this, cursor);
+            checkTwoCharSub<8810,8920>(*this, cursor);
             break;
         case '=':
             checkTwoCharSub<58,8788>(*this, cursor);
@@ -57,9 +77,14 @@ void Cursor::checkForSubstitution(const QChar& c){
             checkTwoCharSub<8656,8660>(*this, cursor);
             checkTwoCharSub<61,8658>(*this, cursor);
             checkTwoCharSub<8866,8614>(*this, cursor);
+            checkTwoCharSub<62,8811>(*this, cursor);
+            checkTwoCharSub<8811,8921>(*this, cursor);
             break;
         case '|':
             checkTwoCharSub<124,8214>(*this, cursor);
+            break;
+        case '~':
+            checkTwoCharSub<126,8776>(*this, cursor);
             break;
         case '0':
             checkTwoCharSub<47,8709>(*this, cursor);
